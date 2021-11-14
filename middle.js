3c854bf283d1328c1899f6d174b3a694ddb7b0b1
Beatrice Yu <88899325+beatrice-yu@users.noreply.github.com>
2021-11-14 18:46:44 -0500

    Update middle.js

--- middle.js
+++ middle.js
@@ -9,8 +9,8 @@ const assertArraysEqual = function(actual, expected) {
 };
 
 const middle = function(x) {
-
-}
+  return (x.length > 2) ? (x.length % 2 === 0) ? [x[Math.floor(x.length / 2) - 1], x[Math.floor(x.length / 2)]] : [x[Math.floor(x.length / 2)]] : [];
+};
 
 //TEST CASE
 console.log(middle([1])); // => []
