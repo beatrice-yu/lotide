a00ce84e1e515807add96887c71087fd1f0af18a
Beatrice Yu <beatricebernadette.yu@gmail.com>
2021-11-21 14:24:44 -0500

    Create findKey.js

---
+++ findKey.js
@@ -0,0 +1,19 @@
+const assertEqual = function(actual, expected) {
+  console.log(
+    (actual === expected) ? `✅✅✅ Assertion Passed: ${actual} === ${expected}` : `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`
+  );
+};
+
+const findKey = function(x, y) {
+  return Object.keys(x).find(key => y(x[key]));
+};
+
+// TEST CASE
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2),"noma");
