c97eec653df0cea6bd6ea348c216004a6b3c1d16
Beatrice Yu <beatricebernadette.yu@gmail.com>
2021-11-21 17:04:07 -0500

    Refactor eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -1,15 +1,5 @@
-const assertEqual = function(actual, expected) {
-  console.log(
-    (actual === expected) ? `✅✅✅ Assertion Passed: ${actual} === ${expected}` : `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`
-  );
-};
-
 const eqArrays = function(x, y) {
   return Array.isArray(x) && Array.isArray(y) && x.length === y.length && x.every((value, i) => value === y[i]);
 };
 
-//TEST CASE
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true); // => false
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true); // => false
+module.exports = eqArrays;
