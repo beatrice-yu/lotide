bfe57ddc683b342a3da01ce305b39969c16fdedb
Beatrice Yu <beatricebernadette.yu@gmail.com>
2021-11-21 13:44:47 -0500

    Update map.js

--- map.js
+++ map.js
@@ -1,3 +1,13 @@
+const eqArrays = function(x, y) {
+  return Array.isArray(x) && Array.isArray(y) && x.length === y.length && x.every((value, i) => value === y[i]);
+};
+
+const assertArraysEqual = function(actual, expected) {
+  console.log(
+    (eqArrays(actual, expected)) ? `✅✅✅ Assertion Passed: ${actual} === ${expected}` : `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`
+  );
+};
+
 const words = ["ground", "control", "to", "major", "tom"];
 
 const map = function(array, callback) {
@@ -9,5 +19,4 @@ const map = function(array, callback) {
 }
 
 //TEST CODE
-const results1 = map(words, word => word[0]);
-console.log(results1);
+console.log(assertArraysEqual(map(words, word => word[0]), words.map(x => x[0])));
